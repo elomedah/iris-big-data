402d89da4d839bd28a991bc7bffad20c2d787a31
elomedah <elomedah@gmail.com>
2021-11-11 23:49:12 +0100

    add hadoop tp

---
+++ TP-hadoop/bash-hadoop-var.sh
@@ -0,0 +1,10 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+export HADOOP_HOME=/home/hdoop/hadoop-3.3.1
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_OPTS=”-Djava.library.path=$HADOOP_HOME/lib/native”
